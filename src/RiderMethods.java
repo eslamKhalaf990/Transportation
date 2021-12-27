c7b45acfb39bf5a1942d19856eac6cf6c2d765fc
eslamKhalaf990 <85128169+eslamKhalaf990@users.noreply.github.com>
2021-12-27 21:08:08 +0200

    Delete src directory

--- src/RiderMethods.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public interface RiderMethods {
-
-     void RequestRide(int index, String source, String destination);
-     void RateDriver(int stars, Driver driver);
-     ArrayList<Notification> getNotificationsList();
-     String toString();
-}
